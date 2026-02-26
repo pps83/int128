4e5adc158e58021111af2df43e631e6c378eb237
Matt Borland <matt@mattborland.com>
2026-02-26 14:31:31 -0500

    Add bit test

--- test/test_bit.cpp
+++ test/test_bit.cpp
@@ -2,10 +2,19 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
+#include <boost/core/lightweight_test.hpp>
+
+#ifndef BOOST_INT128_BUILD_MODULE
+
 #include <boost/int128/int128.hpp>
 #include <boost/int128/bit.hpp>
 #include <boost/int128/iostream.hpp>
-#include <boost/core/lightweight_test.hpp>
+
+#else
+
+import boost.int128;
+
+#endif
 
 void test_has_single_bit()
 {
