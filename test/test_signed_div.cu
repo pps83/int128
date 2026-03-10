7ce948b39a57f6b555167950ff9d806939c5e70a
Matt Borland <matt@mattborland.com>
2026-03-10 16:31:34 -0400

    Improve div and mod testing

--- test/test_signed_div.cu
+++ test/test_signed_div.cu
@@ -26,13 +26,13 @@ using test_type = boost::int128::int128_t;
  * CUDA Kernel Device code
  *
  */
-__global__ void cuda_test(const test_type *in, test_type *out, int numElements)
+__global__ void cuda_test(const test_type *in, const test_type *in2, test_type *out, int numElements)
 {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
 
     if (i < numElements)
     {
-        out[i] = in[i] / in[i];
+        out[i] = in[i] / in2[i];
     }
 }
 
@@ -52,6 +52,7 @@ int main(void)
 
     // Allocate the managed input vector A
     cuda_managed_ptr<test_type> input_vector(numElements);
+    cuda_managed_ptr<test_type> input_vector2(numElements);
 
     // Allocate the managed output vector C
     cuda_managed_ptr<test_type> output_vector(numElements);
@@ -61,6 +62,7 @@ int main(void)
     for (std::size_t i = 0; i < numElements; ++i)
     {
         input_vector[i] = dist(rng);
+        input_vector2[i] = dist(rng);
     }
 
     // Launch the Vector Add CUDA Kernel
@@ -70,7 +72,7 @@ int main(void)
 
     watch w;
 
-    cuda_test<<<blocksPerGrid, threadsPerBlock>>>(input_vector.get(), output_vector.get(), numElements);
+    cuda_test<<<blocksPerGrid, threadsPerBlock>>>(input_vector.get(), input_vector2.get(), output_vector.get(), numElements);
     cudaDeviceSynchronize();
 
     std::cout << "CUDA kernal done in: " << w.elapsed() << "s" << std::endl;
@@ -89,7 +91,7 @@ int main(void)
     w.reset();
     for(int i = 0; i < numElements; ++i)
     {
-        results.push_back(input_vector[i] / input_vector[i]);
+        results.push_back(input_vector[i] / input_vector2[i]);
     }
     double t = w.elapsed();
     // check the results
