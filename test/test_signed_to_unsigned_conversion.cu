d10021af2693c90275cfbb308a0e0848d109d5e5
Matt Borland <matt@mattborland.com>
2026-03-13 08:25:03 -0500

    Add sign conversion define

--- test/test_signed_to_unsigned_conversion.cu
+++ test/test_signed_to_unsigned_conversion.cu
@@ -3,6 +3,8 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#define BOOST_INT128_ALLOW_SIGN_CONVERSION
+
 #include <iostream>
 #include <iomanip>
 #include <vector>
